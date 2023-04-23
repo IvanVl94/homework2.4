8ca8fbd23631ac0b32acc92d787d80730d318c83
IvanVl94 <WlasowIwan@yandex.ru>
2023-04-23 17:23:43 +0300

    Сделано дз2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        User user = new User("qwerty_pro1", "fgrdr4667mlkmkkm", "fgrdr4667");
+        User user = new User("qwerty_pro1=", "fgrdr4667", "fgrdr4667");
         try {
             UserController.getUserInfo(user);
         } catch (Exception e) {
--- src/UserController.java
+++ src/UserController.java
@@ -4,16 +4,17 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
     public static void getUserInfo(User user) {
     try {
-        if (user.getLogin().length() > 20) {
-            throw new WrongLoginException("Логин слишком длинный. Максимальное число символов 20.");
+        if (user.getLogin().length() > 20 || !user.getLogin().matches("[a-zA-Z0-9_]+")) {
+            throw new WrongLoginException("Логин слишком длинный. Максимальное число символов 20. Или указаны недопустимые символы.");
         }
         if (!user.getPassword().equals(user.getConfirmPassword())) {
             throw new WrongPasswordException("Пароли не совпадают.");
         }
-        if (user.getPassword().length() >= 20) {
-            throw new WrongPasswordException("Пароль слишком длинный. Максимальное число символов 19.");
+        if (user.getPassword().length() >= 20 || !user.getPassword().matches("[a-zA-Z0-9_]+")) {
+            throw new WrongPasswordException("Пароль слишком длинный. Максимальное число символов 19. Или указаны недопустимые символы.");
         }
     } catch (WrongLoginException | WrongPasswordException e) {
         System.out.println("Exception: " + e.getMessage());
